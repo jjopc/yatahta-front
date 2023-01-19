ed360f108e7e77525f91c3c6bc073c7f7c4003ae
jjopc <dev.jjopc@gmail.com>
2023-01-19 23:22:23 +0100

    Añado nuevo slice al store para recuperar la lista de pacientes de un doctor

--- src/components/DoctorPatients.jsx
+++ src/components/DoctorPatients.jsx
@@ -1,22 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { getDoctorPatients } from "../services/DoctorService";
 import Layout from "./ui/Layout";
+import { getPatients } from "../state/doctorSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function DoctorPatients() {
   const [patients, setPatients] = useState([]);
 
-  useEffect(() => {
-    const loadPatients = async () => {
-      const { response, isError } = await getDoctorPatients();
-      if (!isError) {
-        setPatients(response.data);
-      } else {
-        setPatients([]);
-      }
-    };
+  const dispatch = useDispatch();
 
-    loadPatients();
-  }, []);
+  // useEffect(() => {
+  //   const loadPatients = async () => {
+  //     const { response, isError } = await getDoctorPatients();
+  //     if (!isError) {
+  //       setPatients(response.data);
+  //     } else {
+  //       setPatients([]);
+  //     }
+  //   };
+
+  //   loadPatients();
+  // }, []);
+
+  useEffect(() => {
+    dispatch(getPatients());
+  }, [dispatch]);
 
   // TODO: Tengo que ir pensando en el context o manejo de estado para no ir pasando constantemente
   // en props si está logueado, etc.
---
+++ src/state/doctorSlice.js
@@ -0,0 +1,53 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { setMessage } from "./messageSlice";
+import { getUser, getAuthHeader } from "../services/AuthService";
+import axios from "axios";
+
+const user = getUser();
+
+export const getPatients = createAsyncThunk(
+  "doctor/getPatients",
+  async (arg, thunkAPI) => {
+    const url = `${import.meta.env.VITE_API_URL}/users/${
+      user.user_id
+    }/patients_list/`;
+    const headers = getAuthHeader();
+    try {
+      const response = await axios.get(url, { headers });
+      return response.data;
+    } catch (error) {
+      console.error("ERROR getPatients en doctorSlice", error);
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      thunkAPI.dispatch(setMessage(message));
+      // TODO: Investigar cómo devolver valores personalizados y poder gestionar el feedback en los formularios
+      // https://redux-toolkit.js.org/api/createAsyncThunk
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+const initialState = [];
+
+const doctorSlice = createSlice({
+  name: "doctor",
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getPatients.fulfilled, (state, action) => {
+        return action.payload;
+      })
+      .addCase(getPatients.rejected, (state, action) => {
+        state = [];
+      });
+  },
+});
+
+export const selectPatientsList = (state) => state.doctor;
+
+export default doctorSlice.reducer;
