c9579423f5e0f4219b5cb78bbf911ea67f582d2b
jjopc <dev.jjopc@gmail.com>
2023-01-20 16:55:07 +0100

    close #1 Muestro la lista de pacientes
    /spent 75m
    
    Cierro esta rama para empezar a implementar lo que falta en esta página

--- src/components/DoctorPatients.jsx
+++ src/components/DoctorPatients.jsx
@@ -48,70 +48,78 @@ export default function DoctorPatients() {
           <Col>
             Introduzca el nombre, ID o código de un usuario para buscarlo
           </Col>
-          <Col>
+          <Col xs="auto">
             {/* TODO: Dirigir a /add-patient */}
             <Button onClick={() => navigate("/")}>Añadir paciente</Button>
           </Col>
         </Row>
         <Row>
-          <Form.Control
-            name="search-text"
-            placeholder="Buscar"
-            id="search-text"
-            className="mt-3 mb-3"
-            onChange={(e) => setSearchText(e.target.value)}
-          />
+          <Col>
+            <Form.Control
+              name="search-text"
+              placeholder="Buscar"
+              id="search-text"
+              className="mt-3 mb-3"
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+          </Col>
         </Row>
         <Row>
-          <Table striped hover bordered responsive="sm">
-            <thead>
-              {/* TODO: Posibilidad de implementar ordenación de la tabla al pulsar un header */}
-              <tr>
-                <th>ID Paciente</th>
-                <th>Código Paciente</th>
-                <th>Nombre de usuario</th>
-                <th>Fecha de nacimiento</th>
-                <th>Puntuación</th>
-                <th>Acciones</th>
-              </tr>
-            </thead>
-            <tbody>
-              {filteredPatients.map((user, index) => {
-                let date = new Date(user.birthday).toLocaleDateString("es", {
-                  day: "2-digit",
-                  month: "2-digit",
-                  year: "numeric",
-                });
-                return (
-                  <tr key={user.id}>
-                    <td>{user.id}</td>
-                    <td>{user.patient_code}</td>
-                    <td>{user.username}</td>
-                    <td>{date}</td>
-                    <td>{user.points}</td>
-                    <td>
-                      <ButtonGroup>
-                        {/* TODO: Implementar estas acciones */}
-                        <Button id="info" size="sm" variant="outline-primary">
-                          Información
-                        </Button>
-                        <Button id="drugs" size="sm" variant="outline-primary">
-                          Medicación
-                        </Button>
-                        <Button
-                          id="messages"
-                          size="sm"
-                          variant="outline-primary"
-                        >
-                          Mensajes
-                        </Button>
-                      </ButtonGroup>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
+          <Col>
+            <Table striped hover bordered responsive="sm">
+              <thead>
+                {/* TODO: Posibilidad de implementar ordenación de la tabla al pulsar un header */}
+                <tr>
+                  <th>ID Paciente</th>
+                  <th>Código Paciente</th>
+                  <th>Nombre de usuario</th>
+                  <th>Fecha de nacimiento</th>
+                  <th>Puntuación</th>
+                  <th>Acciones</th>
+                </tr>
+              </thead>
+              <tbody>
+                {filteredPatients.map((user, index) => {
+                  let date = new Date(user.birthday).toLocaleDateString("es", {
+                    day: "2-digit",
+                    month: "2-digit",
+                    year: "numeric",
+                  });
+                  return (
+                    <tr key={user.id}>
+                      <td>{user.id}</td>
+                      <td>{user.patient_code}</td>
+                      <td>{user.username}</td>
+                      <td>{date}</td>
+                      <td>{user.points}</td>
+                      <td>
+                        <ButtonGroup>
+                          {/* TODO: Implementar estas acciones */}
+                          <Button id="info" size="sm" variant="outline-primary">
+                            Información
+                          </Button>
+                          <Button
+                            id="drugs"
+                            size="sm"
+                            variant="outline-primary"
+                          >
+                            Medicación
+                          </Button>
+                          <Button
+                            id="messages"
+                            size="sm"
+                            variant="outline-primary"
+                          >
+                            Mensajes
+                          </Button>
+                        </ButtonGroup>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </Col>
         </Row>
       </Container>
     </Layout>
