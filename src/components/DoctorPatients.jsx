9fe352c0118ed28cf10266a157c00567d34072be
jjopc <dev.jjopc@gmail.com>
2023-01-20 11:49:53 +0100

    #1 Muestro la lista de pacientes
    Implemento el filtro de la tabla
    
    TODO:
     - Botón Añadir paciente
     - Botones de Acción de la tabla
     - Posibilidad de ordenación al tocar un header de la tabla

--- src/components/DoctorPatients.jsx
+++ src/components/DoctorPatients.jsx
@@ -1,36 +1,119 @@
 import React, { useEffect, useState } from "react";
-import { getDoctorPatients } from "../services/DoctorService";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import Layout from "./ui/Layout";
-import { getPatients } from "../state/doctorSlice";
+import { getPatients, selectPatientsList } from "../state/doctorSlice";
 import { useDispatch, useSelector } from "react-redux";
+import {
+  Container,
+  Row,
+  Col,
+  Breadcrumb,
+  Table,
+  Button,
+  Form,
+  ButtonGroup,
+} from "react-bootstrap";
 
 export default function DoctorPatients() {
-  const [patients, setPatients] = useState([]);
-
+  const [searchText, setSearchText] = useState("");
   const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   const loadPatients = async () => {
-  //     const { response, isError } = await getDoctorPatients();
-  //     if (!isError) {
-  //       setPatients(response.data);
-  //     } else {
-  //       setPatients([]);
-  //     }
-  //   };
-
-  //   loadPatients();
-  // }, []);
+  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(getPatients());
   }, [dispatch]);
 
-  // TODO: Tengo que ir pensando en el context o manejo de estado para no ir pasando constantemente
-  // en props si está logueado, etc.
+  const patients = useSelector(selectPatientsList);
+  const orderedPatients = patients.slice().sort((a, b) => a.points < b.points);
+  let filteredPatients = orderedPatients;
+  if (searchText !== "") {
+    filteredPatients = orderedPatients.filter((patient) => {
+      return (
+        patient.username.toLowerCase().indexOf(searchText) > -1 ||
+        patient.patient_code.toLowerCase().indexOf(searchText) > -1 ||
+        patient.id.toString().indexOf(searchText) > -1
+      );
+    });
+  }
+
   return (
     <Layout>
-      <h1>Lista de pacientes</h1>
+      <Breadcrumb>
+        <Breadcrumb.Item href="/#/">Inicio</Breadcrumb.Item>
+        <Breadcrumb.Item active>Pacientes</Breadcrumb.Item>
+      </Breadcrumb>
+      <Container>
+        <h2>Tus pacientes</h2>
+        <Row>
+          <Col>
+            Introduzca el nombre, ID o código de un usuario para buscarlo
+          </Col>
+          <Col>
+            {/* TODO: Dirigir a /add-patient */}
+            <Button onClick={() => navigate("/")}>Añadir paciente</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Form.Control
+            name="search-text"
+            placeholder="Buscar"
+            id="search-text"
+            className="mt-3 mb-3"
+            onChange={(e) => setSearchText(e.target.value)}
+          />
+        </Row>
+        <Row>
+          <Table striped hover bordered responsive="sm">
+            <thead>
+              {/* TODO: Posibilidad de implementar ordenación de la tabla al pulsar un header */}
+              <tr>
+                <th>ID Paciente</th>
+                <th>Código Paciente</th>
+                <th>Nombre de usuario</th>
+                <th>Fecha de nacimiento</th>
+                <th>Puntuación</th>
+                <th>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredPatients.map((user, index) => {
+                let date = new Date(user.birthday).toLocaleDateString("es", {
+                  day: "2-digit",
+                  month: "2-digit",
+                  year: "numeric",
+                });
+                return (
+                  <tr key={user.id}>
+                    <td>{user.id}</td>
+                    <td>{user.patient_code}</td>
+                    <td>{user.username}</td>
+                    <td>{date}</td>
+                    <td>{user.points}</td>
+                    <td>
+                      <ButtonGroup>
+                        {/* TODO: Implementar estas acciones */}
+                        <Button id="info" size="sm" variant="outline-primary">
+                          Información
+                        </Button>
+                        <Button id="drugs" size="sm" variant="outline-primary">
+                          Medicación
+                        </Button>
+                        <Button
+                          id="messages"
+                          size="sm"
+                          variant="outline-primary"
+                        >
+                          Mensajes
+                        </Button>
+                      </ButtonGroup>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Row>
+      </Container>
     </Layout>
   );
 }
